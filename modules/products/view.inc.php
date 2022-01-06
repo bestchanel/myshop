0308f9086827913c28232c1da7a8c4885fc00b8c
NonNie EiEi <54761564+Nonzaioza71@users.noreply.github.com>
2022-01-06 20:58:50 +0700

    hotfix_sidebar

--- modules/products/view.inc.php
+++ modules/products/view.inc.php
@@ -33,76 +33,76 @@ function numberWithCommas(x) {
                     </div> -->
                     <ul class="navbar-nav" style="width: -webkit-fill-available;">
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_hover btn_filter <?php if($_GET['product_group'] == ""){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_hover btn_filter <?php if($_GET['product_group'] == ""){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product" >
                                 ทั้งหมด
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_hover btn_filter <?php if($_GET['product_group'] == 'CPU'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_hover btn_filter <?php if($_GET['product_group'] == 'CPU'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=CPU" >
                                 <img src="./templates/download/icon/png/005-cpu.png" alt="" srcset="">
                                 CPU
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'MAINBOARD'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'MAINBOARD'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=MAINBOARD">
                                 <img src="./templates/download/icon/png/004-mainboard.png" alt="" srcset="">
                                 Mainboard
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'VGA'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'VGA'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=VGA">
                                 <img src="./templates/download/icon/png/006-vga-card.png" alt="" srcset="">
                                 VGA
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'RAM'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'RAM'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=RAM">
                                 <img src="./templates/download/icon/png/003-ram.png" alt="" srcset="">
                                 RAM
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'HDD'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'HDD'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=HDD">
                                 <img src="./templates/download/icon/png/007-hdd.png" alt="" srcset="">
                                 HDD
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'SSD'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'SSD'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=SSD">
                                 <img src="./templates/download/icon/png/002-solid-state-drive.png" alt="" srcset="">
                                 SSD
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'POWER_SUPPLY'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'POWER_SUPPLY'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=POWER_SUPPLY">
                                 <img src="./templates/download/icon/png/008-power-supply.png" alt="" srcset="">
                                 Power Supply
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'CASE'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'CASE'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=CASE">
                                 <img src="./templates/download/icon/png/010-computer-case-1.png" alt="" srcset="">
                                 Case
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'CPU_COOLLER'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'CPU_COOLLER'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=CPU_COOLLER">
                                 <img src="./templates/download/icon/png/001-water-cooler.png" alt="" srcset="">
                                 CPU Cooler
                             </a>
                         </li>
 
-                        <li class="nav-item btn btn-outline-secondary btn_hover btn_filter <?php if($_GET['product_group'] == 'MONITOR'){ echo "btn_hl"; } ?>">
+                        <li class="nav-item btn btn-outline-secondary w-100 btn_hover btn_filter <?php if($_GET['product_group'] == 'MONITOR'){ echo "btn_hl"; } ?>">
                             <a class="nav-link active d-flex gap-3" aria-current="page" href="?app=product&product_group=MONITOR">
                                 <img src="./templates/download/icon/png/011-monitor.png" alt="" srcset="">
                                 Monitor

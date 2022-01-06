3af7da90582e7367161236e178132ca75466cb4a
NonNie EiEi <54761564+Nonzaioza71@users.noreply.github.com>
2022-01-06 22:44:41 +0700

    hotfix_signIn/Up_display

--- views/detail/login.inc.php
+++ views/detail/login.inc.php
@@ -1,26 +1,41 @@
 <div class="col-md-12 align-content-center text-center">
-    <div class="card col-md-4 m-auto border border-secondary">
+    <div class="position-absolute" style="background-image:url('./templates/image/banner number2.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center; height: 100vh; width:210vh; filter: blur(50px);"></div>
+    <div class="card col-md-4 m-auto border border- border-secondary mt-5" style="background-color: #ffffff91;">
         <div class="card-body m-auto">
             <div style="text-align: center;">
                 <h1>Login</h1>
             </div>
-            <br>
+            <hr>
             <div class="row col-md-12 m-auto">
-                <div class="col-md-6 ps-0">
-                    <label for="">username</label>
-                    <input type="text" class="form-control" name="username" id="username">
-                </div>
-                <div class="col-md-6 pe-0">
-                    <label for="">password</label>
-                    <input type="password" class="form-control" name="password" id="password">
-                </div>
+                <table>
+                    <tr>
+                        <td>
+                            <h3>
+                                <i class="fas fa-user"></i>
+                            </h3>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="username" id="username">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3>
+                                <i class="fas fa-lock"></i>
+                            </h3>
+                        </td>
+                        <td>
+                            <input type="password" class="form-control" name="password" id="password">
+                        </td>
+                    </tr>
+                </table>
                 <br>
                 <div class="col-md-12" style="margin-top: 10%;">
                     <div class="text-center">
-                        <input type="button" class="btn btn-success" style="width: inherit;" value="Login" onclick="loginUser()">
+                        <input type="button" class="btn btn-success w-100" style="width: inherit;" value="Login" onclick="loginUser()">
                     </div>
                     <div class="text-center">
-                        <input type="button" class="btn btn-secondary mt-3" style="width: inherit;" value="Register" onclick='window.location.href = "?app=register";'>
+                        <input type="button" class="btn btn-secondary w-100 mt-3" style="width: inherit;" value="Register" onclick='window.location.href = "?app=register";'>
                     </div>
                 </div>
             </div>
--- views/detail/register.inc.php
+++ views/detail/register.inc.php
@@ -1,44 +1,86 @@
 <div class="col-md-12">
-    <div class="card col-md-4 m-auto border border-secondary">
+    <div class="position-absolute" style="background-image:url('./templates/image/banner number2.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center; height: 100vh; width:210vh; filter: blur(50px);"></div>
+    <div class="card col-md-4 m-auto border border- border-secondary mt-5" style="background-color: #ffffff91;">
         <div class="card-body m-auto"> 
             <div style="text-align: center;">
                 <h1>Register</h1>
             </div>
-            <br>
+            <hr>
             <div class="row col-md-12">
-                <div class="col-md-6">
-                    <label for="">username</label>
-                    <input type="text" class="form-control" name="username" id="username">
-                </div>
-                <div class="col-md-6">
-                    <label for="">password</label>
-                    <input type="password" class="form-control" name="password" id="password">
-                </div>
-                
-                <div class="col-md-6">
-                    <label for="">display name</label>
-                    <input type="text" class="form-control" name="user_name" id="user_name">
-                </div>
-                <div class="col-md-6">
-                    <label for="">phone</label>
-                    <input type="text" class="form-control" name="user_phone" id="user_phone">
-                </div>
-                
-                <div class="col-md-6">
-                    <label for="">Email</label>
-                    <input type="email  " class="form-control" name="user_mail" id="user_mail">
-                </div>
-                <div class="col-md-6">
-                    <label for="">Address</label>
-                    <input type="text" class="form-control" name="user_address" id="user_address">
-                </div>
-                <br>
+                <table>
+                    <tr>
+                        <td>
+                            <h3>
+                                <i class="fas fa-user"></i>
+                            </h3>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" placeholder="Username" name="username" id="username">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3>
+                                <i class="fas fa-lock"></i>
+                            </h3>
+                        </td>
+                        <td>
+                            <input type="password" class="form-control" placeholder="Password" name="password" id="password">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3>
+                                <i class="fas fa-tag"></i>
+                            </h3>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" placeholder="Display name" name="user_name" id="user_name">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3>
+                                <i class="fas fa-phone"></i>
+                            </h3>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" placeholder="Phone" name="user_phone" id="user_phone">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3>
+                                <i class="fas fa-envelope"></i>
+                            </h3>
+                        </td>
+                        <td>
+                            <input type="email  " class="form-control" placeholder="Email" name="user_mail" id="user_mail">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3>
+                                <i class="fas fa-map-marked-alt"></i>
+                            </h3>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" placeholder="Address" name="user_address" id="user_address">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        </td>
+                        <td>
+                        </td>
+                    </tr>
+                </table>
                 <div class="col-md-12" style="margin-top: 10%;">
                     <div class="text-center">
-                        <input type="button" class="btn btn-success" style="width: inherit;" value="Submit" onclick="registerUser()">
+                        <input type="button" class="btn btn-success w-100" style="width: inherit;" value="Submit" onclick="registerUser()">
                     </div>
                     <div class="text-center">
-                        <input type="button" class="btn btn-primary mt-3" style="width: inherit;" value="Login" onclick='window.location.href = "?app=login";'>
+                        <input type="button" class="btn btn-primary w-100 mt-3" style="width: inherit;" value="Login" onclick='window.location.href = "?app=login";'>
                     </div>
                 </div>
             </div>
